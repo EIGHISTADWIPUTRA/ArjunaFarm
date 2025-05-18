c8ff9c6a91f421af8cb6fce5109977ed32700d75
Daneswara <150428419+codewara@users.noreply.github.com>
2025-05-19 06:55:15 +0700

    Update Dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,6 +14,10 @@ export default {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
             },
+            colors: {
+                primary: '#C69C58',
+                secondary: '#2BA468',
+            },
         },
     },
 

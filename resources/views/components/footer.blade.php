8db224d093bfe09436c6b3f684a9967f9a4f9aee
Daneswara <150428419+codewara@users.noreply.github.com>
2025-05-19 07:07:31 +0700

    Fix light mode footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-800 text-white py-8">
+<footer class="bg-white dark:bg-gray-800 text-white py-8">
     <div class="container mx-auto text-center">
         <p>&copy; {{ date('Y') }} Arjuna Farm. All rights reserved.</p>
     </div>

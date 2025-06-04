e7fcde16fa49bbf35835fd6eb5c06d45c725596a
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-04 21:33:05 +0700

    Fix card components (for now)

---
+++ resources/views/components/feature-card.blade.php
@@ -0,0 +1,11 @@
+@props([
+    'xp' => 'p-4',
+    'rounded' => 'rounded-lg',
+])
+
+<div {{ $attributes->merge(['class' =>
+        'flex bg-white text-black dark:bg-gray-800 dark:text-white shadow-md ' .
+        $xp . ' ' . $rounded
+    ]) }}>
+    {{ $slot }}
+</div>

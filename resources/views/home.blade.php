92941f9ec5b7c46c8490d9859fbca06c0e8ed84f
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-15 23:49:31 +0700

    responsive homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,11 +19,11 @@
             </x-product-card>
         </div>
     </section>
-    <section id="about" class="flex px-40 py-8 gap-32">
-        <div class="flex flex-col items-center w-full">
-            <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="h-[250px] hidden lg:block">
-            <img src="{{ asset('/images/text-white.png') }}" alt="Logo Text" class="h-[100px] hidden dark:block">
-            <img src="{{ asset('/images/text-black.png') }}" alt="Logo Text" class="h-[100px] dark:hidden block">
+    <section id="about" class="flex flex-col lg:flex-row px-16 lg:px-40 py-8 gap-8 lg:gap-32">
+        <div class="flex flex-col md:flex-row lg:flex-col justify-center items-center w-full md:gap-4">
+            <img src="{{ asset('/images/logo.png') }}" alt="Logo" class="md:h-[150px] lg:h-[250px] hidden md:block lg:block">
+            <img src="{{ asset('/images/text-white.png') }}" alt="Logo Text" class="md:h-[150px] lg:h-[100px] hidden dark:block">
+            <img src="{{ asset('/images/text-black.png') }}" alt="Logo Text" class="md:h-[150px] lg:h-[100px] dark:hidden block">
         </div>
         <div class="flex flex-col gap-4 w-full">
             <div>
@@ -209,23 +209,31 @@
             </a>
         </div>
     </section>
-    <section id="location" class="flex p-16 pt-8 gap-16">
+    <section id="location" class="flex flex-col md:flex-row lg:flex-row p-16 pt-8 gap-16">
         <iframe loading="lazy" referrerpolicy="no-referrer-when-downgrade" class="w-full h-64 rounded-lg shadow-lg"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15826.768838028895!2d108.2454078!3d-7.3883462!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f5936962ed019%3A0x1a66f58a88bad364!2sArjuna%20Farm!5e0!3m2!1sid!2sid!4v1701657595972!5m2!1sid!2sid">
         </iframe>
         <div class="flex gap-5 w-full text-black dark:text-white">
-            <ul class="font-semibold space-y-4">
-                <li>ADDRESS</li>
-                <li>E-MAIL</li>
-                <li>SITE</li>
-                <li>PHONE</li>
-            </ul>
-            <ul class="space-y-4">
-                <li>Situ Cibeureum Kota Tasikmalaya</li>
-                <li>arjunafarmtasik@gmail.com</li>
-                <li>http://www.arjunafarm.id</li>
-                <li>082119909719</li>
-            </ul>
+            <div class="w-full">
+                <ul class="font-semibold space-y-4">
+                    <li class="flex">
+                        <span class="min-w-[80px]">ADDRESS</span>
+                        <span class="ml-4 break-words">Situ Cibeureum Kota Tasikmalaya</span>
+                    </li>
+                    <li class="flex">
+                        <span class="min-w-[80px]">E-MAIL</span>
+                        <span class="ml-4 break-words">arjunafarmtasik@gmail.com</span>
+                    </li>
+                    <li class="flex">
+                        <span class="min-w-[80px]">SITE</span>
+                        <span class="ml-4 break-words">http://www.arjunafarm.id</span>
+                    </li>
+                    <li class="flex">
+                        <span class="min-w-[80px]">PHONE</span>
+                        <span class="ml-4 break-words">082119909719</span>
+                    </li>
+                </ul>
+            </div>
         </div>
     </section>
 </x-main-layout>

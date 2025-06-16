586026b41ba32c126ddc2b83950dd25a0474cf6a
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-16 14:47:01 +0700

    remove price column

--- database/migrations/2025_06_15_110508_create_transaction_details_table.php
+++ database/migrations/2025_06_15_110508_create_transaction_details_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->foreignId('transaction_id')->constrained('transactions')->onDelete('cascade');
             $table->foreignId('product_id')->constrained('products')->onDelete('cascade');
             $table->integer('quantity');
-            $table->decimal('price', 10, 2);
             $table->timestamps();
         });
     }

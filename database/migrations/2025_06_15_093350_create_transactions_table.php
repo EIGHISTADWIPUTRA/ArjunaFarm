d6f2572e43601a249eab382c06b2bf55653b3d4d
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-16 14:18:41 +0700

    save order id column for invoices

--- database/migrations/2025_06_15_093350_create_transactions_table.php
+++ database/migrations/2025_06_15_093350_create_transactions_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('transactions', function (Blueprint $table) {
             $table->id();
+            $table->string('order_id')->unique();
             $table->string('name');
             $table->string('phone');
             $table->string('email');

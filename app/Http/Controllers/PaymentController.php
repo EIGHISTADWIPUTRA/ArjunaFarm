ca886cafde25df4d3b466e152cdb2421fefd5957
Daneswara <150428419+codewara@users.noreply.github.com>
2025-06-16 15:34:44 +0700

    secure invoicing and prevent redundant payments

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Product;
+use App\Models\Transaction;
+use App\Models\TransactionDetail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 use Midtrans\Snap;
 use Midtrans\Notification;
 
@@ -20,10 +25,17 @@ class PaymentController extends Controller
 
     public function createTransaction(Request $request)
     {
+        $data = new \stdClass();
+        $data->order_id = date('Ymd') . str_pad(Transaction::whereDate('created_at', now()->toDateString())->count() + 1, 3, '0', STR_PAD_LEFT);
+        $data->total = $request->amount;
+        $data->name = $request->name;
+        $data->phone = $request->phone;
+        $data->email = $request->email;
+
         // Create a transaction
         $params = [
             'transaction_details' => [
-                'order_id' => uniqid(),
+                'order_id' => $data->order_id,
                 'gross_amount' => $request->amount,
             ],
             'customer_details' => [
@@ -33,11 +45,50 @@ class PaymentController extends Controller
                 'phone' => $request->phone,
             ],
         ];
+    
+        Transaction::create([
+            'order_id' => $data->order_id,
+            'name' => $data->name,
+            'phone' => $data->phone,
+            'email' => $data->email,
+            'status' => 'pending',
+            'total_amount' => $data->total,
+            'token' => Snap::getSnapToken($params),
+            'secure_key' => Str::random(48),
+        ]);
 
-        $snapToken = Snap::getSnapToken($params);
+        $items = collect(json_decode($request->items))->where('quantity', '>', 0)->values()->all();
+        $products = Product::whereIn('id', collect($items)->pluck('id'))->get();
+        foreach ($products as $product) {
+            $item = collect($items)->firstWhere('id', $product->id);
+            $product->quantity = $item->quantity ?? 0;
+            TransactionDetail::create([
+                'transaction_id' => Transaction::where('order_id', $data->order_id)->first()->id,
+                'product_id' => $product->id,
+                'quantity' => $product->quantity,
+            ]);
+        }
+
+        $transaction = Transaction::where('order_id', $data->order_id)->first();
+
+        // Redirect to invoice page with order_id, so refreshing won't create a new transaction
+        return redirect()->route('invoice.show', ['id' => $transaction->secure_key]);
+    }
+
+    public function showInvoice($id)
+    {
+        $transaction = Transaction::where('secure_key', $id)->first();
+        if (!$transaction) abort(404, 'Transaction not found');
+
+        $details = DB::table('transaction_details as td')
+            ->select('td.transaction_id', 'p.name', 'p.description', 'p.price', 'td.quantity')
+            ->where('td.transaction_id', $transaction->id)
+            ->join('products as p', 'td.product_id', '=', 'p.id')
+            ->get();
 
         return view('invoice', [
-            'snapToken' => $snapToken,
+            'data' => $transaction,
+            'order_details' => $details,
         ]);
     }
 
